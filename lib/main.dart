798e300bae3f3430c5ef76d8ca8d4d72b5b2b07e
Prat2030 <pratyushjindal2003@gmail.com>
2022-12-08 12:49:05 +0530

    divided app into pieces

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_amazon_clone/constants/global_variables.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,14 +12,25 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Amazon Clone',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+          scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+          colorScheme: ColorScheme.light(
+            primary: GlobalVariables.secondaryColor,
+          ),
+          appBarTheme: const AppBarTheme(
+            elevation: 0,
+            iconTheme: IconThemeData(color: Colors.black),
+          )),
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Amazon Clone'),
         ),
-        body: Center(
-          child: Text('Flutter Demo Home Page'),
+        body: Column(
+          children: [
+            const Center(
+              child: Text('Flutter Demo Home Page'),
+            ),
+            ElevatedButton(onPressed: () {}, child: Text('Hello!!'))
+          ],
         ),
       ),
     );
