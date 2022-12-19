615a58e1863e966535cedef0148d78d8029a899a
Prat2030 <pratyushjindal2003@gmail.com>
2022-12-19 22:00:43 +0530

    created user model and the route

--- server/models/user.js
+++ server/models/user.js
@@ -1,1 +1,43 @@
 const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        validate: {
+            validator: (value) => {
+                const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                return value.match(re);
+            },
+            message: "Invalid email address"
+        }
+    },
+    password: {
+        required: true,
+        type: String,
+        validate: {
+            validator: (value) => {
+                return value.length >= 6;
+            },
+            message: "Please enter password with at least 6 characters"
+        }
+    },
+    address:{
+        type: String,
+        default: '',
+    },
+    type:{
+        type: String,
+        default: 'user',
+    },
+    // Cart
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,11 +1,18 @@
 const express = require("express");
+const User = require("../models/user");
 const authRouter = express.Router();
 
-authRouter.post('/api/signup', (req, res) => {
+authRouter.post('/api/signup', async (req, res) => {
     // get the data from the client
     const {name,email,password} = req.body;
+    const existingUser = await User.findOne({email}); // here findOne is a promise
+    if(existingUser){
+        return res.status(400).json({message: "User already exists"});
+    }
+    let user = new User({name,email,password});
+    user = await user.save();
+    res.status(201).json({message: "Account created successfully"});
     // post the data to the database
-    
     // send a response to the client
 });
 
